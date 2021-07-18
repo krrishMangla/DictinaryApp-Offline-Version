3f971fbecda60199f0ea1b269c0715645293a4c2
Krrish Mangla <krrishmangla@gmail.com>
2021-07-18 19:30:50 +0530

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import { Header } from 'react-native-elements';
+import dictionary from '../database';
+
+export default class HomeScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      isSearchPressed: false,
+      isLoading: false,
+      word: 'Loading...',
+      lexicalCategory: '',
+      definition: '',
+    };
+  }
+
+  getWord = (text) => {
+    var txt = text.toLowerCase();
+    try {
+      var word = dictionary[txt]['word'];
+      var definition = dictionary[txt]['definition'];
+      var lexicalCategory = dictionary[txt]['lexicalCategory'];
+      this.setState({
+        word: word,
+        definition: definition,
+        lexicalCategory: lexicalCategory,
+      });
+    } catch (err) {
+      alert('Sorry this word is not available now');
+      this.setState({
+        text: '',
+        isSearchPressed: false,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, borderWidth: 2,    backgroundColor: '#b8b8b8' }}>
+      <Header
+          backgroundColor={'red'}
+          centerComponent={{
+            text: 'Pocket Dictionary App',
+            style: { color: '#fff', fontSize: 20 },
+          }}
+        />
+
+        <Image
+          style={{ marginTop: 30, width: 150, height: 150, marginLeft: 100 }}
+          source={{
+            uri:
+              'https://www.educationworld.com/a_lesson/lesson/images/vocabulary.gif',
+          }}
+        />
+
+        <View style={styles.inputBoxContainer}>
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={(text) => {
+              this.setState({
+                text: text,
+                isSearchPressed: false,
+                word: 'Loading...',
+                lexicalCategory: '',
+                examples: [],
+                definition: '',
+              });
+            }}
+            value={this.state.text}
+          />
+
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => {
+              this.setState({ isSearchPressed: true });
+              this.getWord(this.state.text);
+            }}>
+            <Text style={styles.searchText}>Search</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.outputContainer}>
+          <Text style={{ fontSize: 20 }}>
+            {this.state.isSearchPressed && this.state.word === 'Loading...'
+              ? this.state.word: ''}
+          </Text>
+          {this.state.word !== 'Loading...' ? (
+            <View style={{ justifyContent: 'center', marginLeft: 10 }}>
+              <View style={styles.detailsContainer}>
+                <Text style={styles.detailsTitle}>Word : </Text>
+                <Text style={{ fontSize: 18 }}>{this.state.word}</Text>
+              </View>
+              <View style={styles.detailsContainer}>
+                <Text style={styles.detailsTitle}>Type : </Text>
+                <Text style={{ fontSize: 18 }}>
+                  {this.state.lexicalCategory}
+                </Text>
+              </View>
+              <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                <Text style={styles.detailsTitle}>Definition : </Text>
+                <Text style={{ fontSize: 18 }}>{this.state.definition}</Text>
+              </View>
+            </View>
+          ) : null}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputBoxContainer: {
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputBox: {
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+  },
+  searchButton: {
+    width: '40%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+  },
+  searchText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  outputContainer: {
+    flex: 0.7,
+    alignItems: 'center',
+  },
+  detailsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  detailsTitle: {
+    color: 'orange',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
